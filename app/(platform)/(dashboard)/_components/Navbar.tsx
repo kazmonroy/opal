6cf3b550871fecda3c67cc27ad02bec690d0f170
kazmonroy <katherinemonroy@gmail.com>
2024-08-19 00:02:01 +0200

    Add mobile navbar

--- app/(platform)/(dashboard)/_components/Navbar.tsx
+++ app/(platform)/(dashboard)/_components/Navbar.tsx
@@ -2,11 +2,13 @@ import Logo from '@/components/logo';
 import { Button } from '@/components/ui';
 import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
 import { Plus } from 'lucide-react';
+import MobileSidebar from './mobile-sidebar';
 
 function Navbar() {
   return (
     <nav className='fixed px-4 z-50 top-0 w-full h-14 border-b shadow-sm bg-slate-50 flex items-center'>
       {/* TODO: Mobile Sidebar */}
+      <MobileSidebar />
 
       <div className='flex items-center gap-x-4'>
         <div className='hidden md:flex'>
