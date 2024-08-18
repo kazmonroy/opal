5e116ba4e0fc4dd9996deb1bdd8a230d1022e6c5
kazmonroy <katherinemonroy@gmail.com>
2024-08-18 02:49:08 +0200

    Add footer

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className='bg-slate-50 text-slate-500 fixed bottom-0 w-full'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-center border-t border-slate-400/10 py-10'>
+          <p>Copyright © 2024 Opal. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,13 @@
 import Logo from '@/components/logo';
 import Navbar from './_components/navbar';
+import Footer from './_components/footer';
 
 function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='h-full bg-slate-50'>
       <Navbar />
       <main className='pt-40 pb-20 bg-slate-50'>{children}</main>
+      <Footer />
     </div>
   );
 }
