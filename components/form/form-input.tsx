576b55d3b4d0a1735baaebde936be3509c583cbe
kazmonroy <katherinemonroy@gmail.com>
2024-08-22 00:08:33 +0200

    Create form input component

---
+++ components/form/form-input.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { useFormStatus } from 'react-dom';
+import { Input, Label } from '@/components/ui';
+import { cn } from '@/lib/utils';
+
+interface FormInputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  required?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  defaultValue?: string;
+  onBlur?: () => void;
+}
+
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+  (
+    {
+      id,
+      label,
+      type,
+      placeholder,
+      disabled,
+      required,
+      errors,
+      className,
+      defaultValue = '',
+      onBlur,
+    },
+    ref
+  ) => {
+    const { pending } = useFormStatus();
+
+    return (
+      <div className='space-y-2'>
+        <div className='space-y-1'>
+          {label && (
+            <Label
+              htmlFor={id}
+              className='text-sm font-semibold text-slate-700'
+            >
+              {label}
+            </Label>
+          )}
+          <Input
+            id={id}
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn('text-sm px-2 py-1 h-7', className)}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
+      </div>
+    );
+  }
+);
+
+FormInput.displayName = 'FormInput';
--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,6 +2,7 @@ export { Button } from '@/components/ui/button';
 export { Separator } from '@/components/ui/separator';
 export { Skeleton } from '@/components/ui/skeleton';
 export { Input } from '@/components/ui/input';
+export { Label } from '@/components/ui/label';
 
 export {
   Accordion,
