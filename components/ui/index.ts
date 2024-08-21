4c44c0ddd79c55cc7199beeca016f999f0f0b11a
kazmonroy <katherinemonroy@gmail.com>
2024-08-21 22:17:58 +0200

    Add Input component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,7 @@
 export { Button } from '@/components/ui/button';
 export { Separator } from '@/components/ui/separator';
 export { Skeleton } from '@/components/ui/skeleton';
+export { Input } from '@/components/ui/input';
 
 export {
   Accordion,
