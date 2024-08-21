5dbf5c92eaa550e5aad56b3ca07b42fa09310066
kazmonroy <katherinemonroy@gmail.com>
2024-08-21 23:01:32 +0200

    Create Board schema and types

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const createBoardSchema = z.object({
+  title: z.string({ required_error: 'Title is required' }).min(3, {
+    message: 'Title must be at least 3 characters',
+  }),
+});
