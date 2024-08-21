1ccbd8780df0c368e1d6e24a4bc5e5bb0e5e869e
kazmonroy <katherinemonroy@gmail.com>
2024-08-21 23:31:20 +0200

    Add useAction handler

---
+++ hooks/use-action.ts
@@ -0,0 +1,63 @@
+import { ActionState, FieldErrors } from '@/lib/create-safe-action';
+import { useCallback, useState } from 'react';
+
+type Action<TInput, TOutput> = (
+  data: TInput
+) => Promise<ActionState<TInput, TOutput>>;
+
+interface UseActionOptions<TOutput> {
+  onSuccess?: (data: TOutput) => void;
+  onError?: (error: string) => void;
+  onComplete?: () => void;
+}
+
+export function useAction<TInput, TOutput>(
+  action: Action<TInput, TOutput>,
+  options: UseActionOptions<TOutput> = {}
+) {
+  const [fieldErrors, setFieldErrors] = useState<
+    FieldErrors<TInput> | undefined
+  >(undefined);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [data, setData] = useState<TOutput | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const execute = useCallback(
+    async (input: TInput) => {
+      setIsLoading(true);
+      try {
+        const result = await action(input);
+
+        if (!result) {
+          return;
+        }
+
+        if (result.fieldErrors) {
+          setFieldErrors(result.fieldErrors);
+        }
+
+        if (result.error) {
+          setError(result.error);
+          options.onError?.(result.error);
+        }
+
+        if (result.data) {
+          setData(result.data);
+          options.onSuccess?.(result.data);
+        }
+      } finally {
+        setIsLoading(false);
+        options.onComplete?.();
+      }
+    },
+    [options, action]
+  );
+
+  return {
+    fieldErrors,
+    error,
+    data,
+    isLoading,
+    execute,
+  };
+}
