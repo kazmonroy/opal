4e9a7ad36543671350eaae62c8eaa0df1d4212ef
kazmonroy <katherinemonroy@gmail.com>
2024-08-22 23:02:07 +0200

    Clean organization page

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,21 +1,8 @@
-import { createBoard } from '@/actions';
-import { Button } from '@/components/ui';
 import { db } from '@/db';
-import Board from './board';
-import Form from './form';
 
 async function OrganziationIdPage() {
   const boards = await db.board.findMany();
-  return (
-    <div className='flex flex-col gap-2'>
-      <Form />
-      <ul className='flex flex-col gap-2 py-2 w-40'>
-        {boards.map((board) => (
-          <Board key={board.id} {...board} />
-        ))}
-      </ul>
-    </div>
-  );
+  return <div className='flex flex-col gap-2'></div>;
 }
 
 export default OrganziationIdPage;
