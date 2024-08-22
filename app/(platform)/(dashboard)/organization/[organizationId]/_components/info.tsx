86ca991d5bc792519a66cb351ae5ebf87ec9c246
kazmonroy <katherinemonroy@gmail.com>
2024-08-22 23:23:08 +0200

    Add Skeleton loading in Info component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Skeleton } from '@/components/ui';
 import { useOrganization } from '@clerk/nextjs';
 import { CreditCard } from 'lucide-react';
 import Image from 'next/image';
@@ -7,7 +8,7 @@ import Image from 'next/image';
 function Info() {
   const { organization, isLoaded } = useOrganization();
   if (!isLoaded) {
-    return <p>Loading...</p>;
+    return <Info.Skeleton />;
   }
   return (
     <div className='flex items-center gap-x-4'>
@@ -30,4 +31,20 @@ function Info() {
   );
 }
 
+Info.Skeleton = function SkeletonInfo() {
+  return (
+    <div className='flex items-center gap-x-4'>
+      <div className='w-[60px] h-[60px] relative'>
+        <Skeleton className='h-full w-full absolute' />
+      </div>
+      <div className='space-y-2'>
+        <Skeleton className='h-10 w-[200px]' />
+        <div className='flex items-center'>
+          <Skeleton className='h-4 w-4 mr-2' />
+          <Skeleton className='h-4 w-[100px]' />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Info;
