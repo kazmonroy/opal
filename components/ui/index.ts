34921be3ea0917215988cb1d37b41a56c479d55b
kazmonroy <katherinemonroy@gmail.com>
2024-08-22 23:48:11 +0200

    Add Tooltip component

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -3,6 +3,12 @@ export { Separator } from '@/components/ui/separator';
 export { Skeleton } from '@/components/ui/skeleton';
 export { Input } from '@/components/ui/input';
 export { Label } from '@/components/ui/label';
+export {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from '@/components/ui/tooltip';
 
 export {
   Accordion,
