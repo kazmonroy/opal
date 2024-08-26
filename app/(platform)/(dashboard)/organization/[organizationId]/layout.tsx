a12ddf26718147d5f484a475dbe5524d64c54930
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 01:00:55 +0200

    Add organization metadata

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,14 @@
+import { auth } from '@clerk/nextjs/server';
+import { startCase } from 'lodash';
 import OrgControl from './_components/org-control';
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || 'organization'),
+  };
+}
 
 function OrganizationIdLayout({ children }: { children: React.ReactNode }) {
   return (
