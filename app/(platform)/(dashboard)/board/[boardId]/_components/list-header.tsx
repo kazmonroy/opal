1847f450af1e5be49c0c19457551dc9ea61a0676
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 20:36:46 +0200

    Update ListHeader style and interface

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-header.tsx
@@ -1,10 +1,16 @@
 'use client';
 
-function ListHeader() {
+import { List } from '@prisma/client';
+
+interface ListHeaderProps {
+  data: List;
+}
+
+function ListHeader({ data }: ListHeaderProps) {
   return (
     <div className='pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2'>
       <div className='w-full text-sm px-2.5 py-1 h-7 font-medium'>
-        List header
+        {data.title}
       </div>
     </div>
   );
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -11,7 +11,7 @@ function ListItem({ data, index }: ListItemProps) {
   return (
     <li className='shrink-0 h-full w-[17rem] select-none'>
       <div className='w-full rounded-md bg-slate-50 shadow-md pb-2'>
-        <ListHeader />
+        <ListHeader data={data} />
       </div>
     </li>
   );
