f85590edc5429b25a820cd8a902c31887ff784ec
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 16:13:33 +0200

    Update list form editing mode

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -3,12 +3,15 @@
 import { ElementRef, useRef, useState } from 'react';
 import { Plus } from 'lucide-react';
 import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+import { useParams } from 'next/navigation';
 
 import { Button } from '@/components/ui';
 import ListWrapper from './list-wrapper';
 import { FormInput } from '@/components/form/form-input';
+import FormSubmit from '@/components/form/form-submit';
 
 function ListForm() {
+  const params = useParams();
   const formrRef = useRef<ElementRef<'form'>>(null);
   const inputRef = useRef<ElementRef<'input'>>(null);
   const [isEditing, setIsEditing] = useState(false);
@@ -46,8 +49,13 @@ function ListForm() {
             id='title'
             defaultValue=''
             placeholder='Enter list title'
-            className='text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
+            className='text-sm bg-slate-50 px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
           />
+          <input type='submit' value={params.boardId} name='boardId' hidden />
+
+          <FormSubmit variant='outline' className='w-full'>
+            Add list
+          </FormSubmit>
         </form>
       </ListWrapper>
     );
