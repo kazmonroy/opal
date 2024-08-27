0485efd50f69720095b83506db5a99d01a4bd723
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 10:14:08 +0200

    Update Board title form

--- app/(platform)/(dashboard)/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/_components/board-title-form.tsx
@@ -1,16 +1,58 @@
 'use client';
 
+import { ElementRef, useRef, useState } from 'react';
 import { Board } from '@prisma/client';
 import { Button } from '@/components/ui';
+import { FormInput } from '@/components/form/form-input';
 
 interface BoardTitleFormProps {
   data: Board;
 }
 function BoardTitleForm({ data: { title } }: BoardTitleFormProps) {
+  const formRef = useRef<ElementRef<'form'>>(null);
+  const inputRef = useRef<ElementRef<'input'>>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const enableEditing = () => {
+    // TODO: Add validation
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    }, 0);
+  };
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get('title') as string;
+    console.log(title);
+    // setIsEditing(false);
+  };
+
+  if (isEditing) {
+    return (
+      <form
+        ref={formRef}
+        action={onSubmit}
+        className='flex items-center gap-x-2'
+      >
+        <FormInput
+          ref={inputRef}
+          id='title'
+          defaultValue={title}
+          onBlur={() => {}}
+          className='font-bold text-lg px-1 py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none'
+        />
+      </form>
+    );
+  }
+
   return (
     <Button
       variant='transparent'
       className='font-bold text-lg h-auto w-auto p-1 px-2'
+      onClick={enableEditing}
     >
       {title}
     </Button>
