64c5bc49be024c9a75971469965551574b95c8af
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 14:48:12 +0200

    Update Board page

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,39 @@
-function BoardIdPage() {
-  return <div>Board Id!</div>;
+import { db } from '@/db';
+import { auth } from '@clerk/nextjs/server';
+import { orderBy } from 'lodash';
+import { redirect } from 'next/navigation';
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+async function BoardIdPage({ params }: BoardIdPageProps) {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect('select-org');
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  });
+
+  return <div className='p-4 w-full h-full overflow-x-auto'>Board Id!</div>;
 }
 
 export default BoardIdPage;
