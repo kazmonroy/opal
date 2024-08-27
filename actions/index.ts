ffb6f07aa0fb3d0716b0d19eb2e73f86c852c5bb
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 21:36:24 +0200

    Update actions index file

--- actions/index.ts
+++ actions/index.ts
@@ -2,4 +2,5 @@ export { createBoard } from './create-board/index';
 export { updateBoard } from './update-board/index';
 export { deleteBoard } from './delete-board/index';
 
+export { createList } from './create-list/index';
 export { updateList } from './update-list/index';
