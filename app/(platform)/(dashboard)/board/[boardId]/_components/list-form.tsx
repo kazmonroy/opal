5eeccef3826275005fdf020e203b92f25b99acd1
kazmonroy <katherinemonroy@gmail.com>
2024-08-27 15:59:12 +0200

    Add form state and structure

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -2,10 +2,11 @@
 
 import { ElementRef, useRef, useState } from 'react';
 import { Plus } from 'lucide-react';
+import { useEventListener, useOnClickOutside } from 'usehooks-ts';
 
 import { Button } from '@/components/ui';
 import ListWrapper from './list-wrapper';
-import { useEventListener, useOnClickOutside } from 'usehooks-ts';
+import { FormInput } from '@/components/form/form-input';
 
 function ListForm() {
   const formrRef = useRef<ElementRef<'form'>>(null);
@@ -32,11 +33,31 @@ function ListForm() {
   useEventListener('keydown', onKeyDown);
   useOnClickOutside(formrRef, disableEditing);
 
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          action=''
+          ref={formrRef}
+          className='w-full p-3 rounded-md bg-slate-50 space-y-4 shadow-md'
+        >
+          <FormInput
+            ref={inputRef}
+            id='title'
+            defaultValue=''
+            placeholder='Enter list title'
+            className='text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition'
+          />
+        </form>
+      </ListWrapper>
+    );
+  }
+
   return (
     <ListWrapper>
-      <Button variant='outline' className='w-full'>
-        <Plus className='w-4 h-4 mr-2' />
+      <Button onClick={enableEditing} variant='outline' className='w-full'>
         Create List
+        <Plus className='w-4 h-4 ml-2' />
       </Button>
     </ListWrapper>
   );
