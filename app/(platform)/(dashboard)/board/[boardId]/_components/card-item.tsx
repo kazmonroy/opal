33a6aa51827256a8c47e9bd0284ac744a53cdefa
katherine.monroy <katherine.monroy@eon.se>
2024-08-29 22:30:41 +0200

    Change CardItem role type

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -8,8 +8,8 @@ interface CardItemProps {
 }
 function CardItem({ data, index }: CardItemProps) {
   return (
-    <li key={data.id} className="w-full">
-      <div className="truncate border-2 border-transparent hover:border-slate-800 py-2 cursor-pointer px-3 text-sm bg-white rounded-md shadow-sm transition">
+    <li key={data.id} className="w-full" role="button">
+      <div className="truncate border-2 border-transparent hover:border-slate-800 py-2 px-3 text-sm bg-white rounded-md shadow-sm transition">
         <p>{data.title}</p>
       </div>
     </li>
