2cb6a453b97d7ef161548679841d5d20b5c96de0
Katherine Monroy <katherinemonroy@Katherines-MacBook-Pro-2.local>
2024-08-29 23:42:49 +0200

    Add @hello-pangea/dnd package

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { ListWithCards } from '@/types';
-import ListForm from './list-form';
-import ListItem from './list-item';
+import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { ListWithCards } from "@/types";
+import ListForm from "./list-form";
+import ListItem from "./list-item";
 
 interface ListContainerProps {
   boardId: string;
@@ -17,12 +18,23 @@ export default function ListContainer({ boardId, data }: ListContainerProps) {
     setOrderedData(data);
   }, [data]);
   return (
-    <ol className='flex gap-x-3 h-full'>
-      {orderedData.map((list, index) => (
-        <ListItem key={list.id} index={index} data={list} />
-      ))}
-      <ListForm />
-      <div className='flex-shrink-0 w-1' />
-    </ol>
+    <DragDropContext onDragEnd={() => {}}>
+      <Droppable droppableId="lists" type="lists" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => (
+              <ListItem key={list.id} index={index} data={list} />
+            ))}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
