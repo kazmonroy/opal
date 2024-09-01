794520f48c7ca3fd1b50f504d9a7566e1880742f
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 00:48:31 +0200

    Create copyCard server action

---
+++ actions/copy-card/index.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
+import { Card } from "@prisma/client";
+import { InputType, ReturnType } from "./types";
+import { copyCardSchema } from "./schema";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { db } from "@/db";
+
+async function hanlder(data: InputType): Promise<ReturnType> {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Not Authorized",
+    };
+  }
+
+  const { id, boardId } = data;
+
+  let card: Card;
+  try {
+    const cardToCopy = await db.card.findUnique({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+
+    if (!cardToCopy) {
+      return {
+        error: "Card not found",
+      };
+    }
+
+    const lastCard = await db.card.findFirst({
+      where: {
+        listId: cardToCopy.listId,
+      },
+      orderBy: {
+        order: "desc",
+      },
+      select: { order: true },
+    });
+
+    const newOrder = lastCard ? lastCard.order + 1 : 1;
+
+    card = await db.card.create({
+      data: {
+        title: `${cardToCopy.title} - Copy`,
+        description: cardToCopy.description,
+        listId: cardToCopy.listId,
+        order: newOrder,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        error: error.message,
+      };
+    } else {
+      return {
+        error: "Failed to copy list",
+      };
+    }
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return {
+    data: card,
+  };
+}
+
+export const copyCard = createSafeAction(copyCardSchema, hanlder);
--- actions/index.ts
+++ actions/index.ts
@@ -2,6 +2,7 @@ export { createBoard } from "./create-board/index";
 export { updateBoard } from "./update-board/index";
 export { deleteBoard } from "./delete-board/index";
 
+export { copyCard } from "./copy-card/index";
 export { createCard } from "./create-card/index";
 export { updateCard } from "./update-card/index";
 export { updateCardOrder } from "./update-card-order/index";
