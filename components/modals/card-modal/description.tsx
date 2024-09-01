59d6e4cb82e370977d3a45b74f814227bd0b6812
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 00:03:07 +0200

    Update Description component onSubmit logic

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,24 +1,38 @@
 "use client";
 
+import { toast } from "sonner";
 import { AlignLeft } from "lucide-react";
 import { useParams } from "next/navigation";
 import { ElementRef, useRef, useState } from "react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { useQueryClient } from "@tanstack/react-query";
 import { Button, Skeleton } from "@/components/ui";
 import { CardWithList } from "@/types";
-import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { FormTextarea } from "@/components/form/form-textarea";
 import FormSubmit from "@/components/form/form-submit";
+import { updateCard } from "@/actions";
+import { useAction } from "@/hooks/use-action";
 
 interface DescriptionProps {
   data: CardWithList;
 }
 function Description({ data }: DescriptionProps) {
-  const queryClient = useQueryClient();
-  const params = useParams();
   const [isEditing, setIsEditing] = useState(false);
   const textareaRef = useRef<ElementRef<"textarea">>(null);
   const formRef = useRef<ElementRef<"form">>(null);
+  const queryClient = useQueryClient();
+  const params = useParams();
+  const { execute, fieldErrors } = useAction(updateCard, {
+    onSuccess: (data) => {
+      toast.success(`Card "${data.title}" updated`);
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      });
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   const enableEditing = () => {
     setIsEditing(true);
@@ -37,20 +51,21 @@ function Description({ data }: DescriptionProps) {
     }
   };
 
-  useEventListener("keydown", onKeyDown);
-  useOnClickOutside(formRef, disableEditing);
-
   const onSubmit = (formData: FormData) => {
     const description = formData.get("description") as string;
     const boardId = params.boardId as string;
+
+    if (!description) return;
 
     if (description === data.description) {
       disableEditing();
     }
 
-    // execute({ id: data.id, boardId, description });
+    execute({ id: data.id, boardId, description });
   };
 
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
   return (
     <div className="flex items-start gap-x-3 w-full justify-start">
       <AlignLeft className="h-5 w-5 mt-0.5 text-slate-500" />
@@ -58,12 +73,13 @@ function Description({ data }: DescriptionProps) {
         <p className="font-semibold text-slate-700 mb-2">Description</p>
 
         {isEditing ? (
-          <form action="" ref={formRef} className="space-y-2">
+          <form action={onSubmit} ref={formRef} className="space-y-2">
             <FormTextarea
               id="description"
               className="w-full mt-2"
               placeholder="Add more details..."
               defaultValue={data.description ?? undefined}
+              errors={fieldErrors}
             />
             <div className="w-full flex justify-end items-center gap-x-2 ">
               <Button
