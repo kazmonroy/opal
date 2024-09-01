750addcf68cac2bca3ea041264de42cd29346938
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-01 23:03:13 +0200

    Add loading Skeleton in Description component

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui";
 import { CardWithList } from "@/types";
 
 interface DescriptionProps {
@@ -8,5 +9,17 @@ interface DescriptionProps {
 function Description({ data }: DescriptionProps) {
   return <div>{data.description}</div>;
 }
+
+Description.Skeleton = function DescriptionSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 w-full">
+      <Skeleton className="h-6 w-6 bg-slate-200" />
+      <div className="w-full">
+        <Skeleton className="h-26 w-24 mb-2 bg-neutral-200" />
+        <Skeleton className="w-full h-[4.5rem] bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
 
 export default Description;
