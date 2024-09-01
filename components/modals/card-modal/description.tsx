1713e6d067d2bc72a071bc49b25fe8e4d987d936
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-01 23:23:58 +0200

    Update Description component style

--- components/modals/card-modal/description.tsx
+++ components/modals/card-modal/description.tsx
@@ -11,8 +11,15 @@ function Description({ data }: DescriptionProps) {
   return (
     <div className="flex items-start gap-x-3 w-full justify-start">
       <AlignLeft className="h-5 w-5 mt-0.5 text-slate-500" />
-      {data.description}
-      Description here
+      <div className="w-full">
+        <p className="font-semibold text-slate-700 mb-2">Description</p>
+        <div
+          role="button"
+          className="min-h-[4.5rem] bg-slate-100 text-slate-500 text-sm font-medium py-3 px-3 rounded-md"
+        >
+          {data.description ?? "Add more details..."}
+        </div>
+      </div>
     </div>
   );
 }
@@ -22,7 +29,7 @@ Description.Skeleton = function DescriptionSkeleton() {
     <div className="flex items-start gap-x-3 w-full">
       <Skeleton className="h-6 w-6 bg-slate-200" />
       <div className="w-full">
-        <Skeleton className="h-26 w-24 mb-2 bg-neutral-200" />
+        <Skeleton className="h-26 w-24 bg-neutral-200" />
         <Skeleton className="w-full h-[4.5rem] bg-neutral-200" />
       </div>
     </div>
