9b52484ad51211d02288a2229b931102cf4e1083
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-01 21:38:28 +0200

    Add submit handler in Header component

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -5,27 +5,44 @@ import { ElementRef, useRef, useState } from "react";
 import { FormInput } from "@/components/form/form-input";
 import { CardWithList } from "@/types";
 import { Skeleton } from "@/components/ui";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 interface HeaderProps {
   data: CardWithList;
 }
 function Header({ data }: HeaderProps) {
-  const { title } = data;
+  const { title, list } = data;
   const inputRef = useRef<ElementRef<"input">>(null);
+  const queryClient = useQueryClient();
+  const params = useParams();
   const [headerTitle, setHeaderTitle] = useState(title);
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const newTitle = formData.get("title") as string;
+
+    console.log("newTitle", newTitle);
+  };
 
   return (
     <div className="flex items-start gap-x-3 mb-6 w-full">
       <Layout className="h-4 w-4 mt-1 text-slate-700 " />
       <div className="w-full">
-        <form action="">
+        <form action={onSubmit}>
           <FormInput
             id="title"
             ref={inputRef}
             defaultValue={headerTitle}
+            onBlur={onBlur}
             className="font-semibold text-xl px-1 text-slate-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-slate-50 focus-visible:border-input mb-0.5 truncate"
           />
         </form>
+        <p className="text-sm text-muted-foreground">
+          in list <span className="underline">{list.title}</span>
+        </p>
       </div>
     </div>
   );
