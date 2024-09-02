09a697e40c9511810a1f66e5dcdea82d170d57e6
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 23:38:41 +0200

    Add lazy loading for ActivityList component

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,11 +1,16 @@
 import { Separator } from "@/components/ui";
 import Info from "../_components/info";
+import { Suspense } from "react";
+import ActivityList from "./_components/activity-list";
 
 function ActivityPage() {
   return (
     <div className="w-full">
       <Info />
       <Separator className="my-4" />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList />
+      </Suspense>
     </div>
   );
 }
