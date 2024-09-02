eb2b541dbfd32935c196b2a95cd404c0b4336ffb
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 23:32:49 +0200

    Add loading Skeleton in ActivityList

--- app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/_components/activity-list.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import ActivityItem from "@/components/activity-item";
 import { db } from "@/db";
+import { Skeleton } from "@/components/ui";
 
 async function ActivityList() {
   const { orgId } = auth();
@@ -26,5 +27,17 @@ async function ActivityList() {
     </ol>
   );
 }
+
+ActivityList.Skeleton = function ActivityListSkeleton() {
+  return (
+    <ol className=" space-y-4 mt-4">
+      <Skeleton className="w-[80%] h-14 " />
+      <Skeleton className="w-[50%] h-14 " />
+      <Skeleton className="w-[80%] h-14 " />
+      <Skeleton className="w-[60%] h-14 " />
+      <Skeleton className="w-[80%] h-14 " />
+    </ol>
+  );
+};
 
 export default ActivityList;
