92872d4fdce9aecd4bab1cb685578326228c9f57
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 23:20:31 +0200

    Add Activity Page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,13 @@
+import { Separator } from "@/components/ui";
+import Info from "../_components/info";
+
+function ActivityPage() {
+  return (
+    <div className="w-full">
+      <Info />
+      <Separator className="my-4" />
+    </div>
+  );
+}
+
+export default ActivityPage;
