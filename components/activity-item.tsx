a50eb1fdbe627edd9cb5328b4db8708cba4b0ea4
Katherine Monroy <katherinemonroy@gmail.com>
2024-09-02 22:18:02 +0200

    Update ActivityItem component structure

--- components/activity-item.tsx
+++ components/activity-item.tsx
@@ -1,10 +1,26 @@
 import { AuditLog } from "@prisma/client";
+import { Avatar, AvatarImage } from "./ui";
+import { generateLogMessage } from "@/lib/generate-log-message";
 
 interface ActivityItemProps {
   data: AuditLog;
 }
 function ActivityItem({ data }: ActivityItemProps) {
-  return <div></div>;
+  return (
+    <li className="flex items-center gap-x-2">
+      <Avatar className="h-8 w-8">
+        <AvatarImage src={data.userImage} />
+      </Avatar>
+      <div className="flex flex-col space-y-0.5">
+        <p className="text-sm text-muted-foreground">
+          <span className="font-semibold lowercase text-slate-700">
+            {data.userName}
+          </span>
+          {generateLogMessage(data)}
+        </p>
+      </div>
+    </li>
+  );
 }
 
 export default ActivityItem;
