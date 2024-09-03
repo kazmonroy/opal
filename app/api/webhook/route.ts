0acc42dc8b285d1592aaf9ee4d7c86bc02b7513f
kazmonroy <katherinemonroy@gmail.com>
2024-09-04 00:12:25 +0200

    Update Strip webhook and middleware

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,14 +1,14 @@
-import Stripe from "stripe";
-import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import Stripe from 'stripe';
+import { headers } from 'next/headers';
+import { NextResponse } from 'next/server';
 
-import { db } from "@/db";
-import { stripe } from "@/lib/stripe";
+import { db } from '@/db';
+import { stripe } from '@/lib/stripe';
 
 export async function POST(req: Request) {
   const body = await req.text();
 
-  const signature = headers().get("Stripe-Signature") as string;
+  const signature = headers().get('Stripe-Signature') as string;
 
   let event: Stripe.Event;
   try {
@@ -18,18 +18,18 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     );
   } catch (error) {
-    return new NextResponse("Invalid signature", { status: 400 });
+    return new NextResponse('Invalid signature', { status: 400 });
   }
 
   //   Event after the ckeckout session is completed
   const session = event.data.object as Stripe.Checkout.Session;
-  if (event.type === "checkout.session.completed") {
+  if (event.type === 'checkout.session.completed') {
     const orgSubscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
 
     if (!session?.metadata?.orgId) {
-      return new NextResponse("OrgId not found", { status: 400 });
+      return new NextResponse('OrgId not found', { status: 400 });
     }
 
     await db.orgSubscription.create({
@@ -45,7 +45,7 @@ export async function POST(req: Request) {
     });
   }
 
-  if (event.type === "invoice.payment_succeeded") {
+  if (event.type === 'invoice.payment_succeeded') {
     const orgSubscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/api/webhook',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+]);
 
 export default clerkMiddleware((auth, request) => {
   const { userId, orgId } = auth();
