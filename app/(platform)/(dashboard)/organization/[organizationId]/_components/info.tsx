f59b4c75bfe9d5aca0dc1a0a000808cb79c63b08
kazmonroy <katherinemonroy@gmail.com>
2024-09-04 00:35:25 +0200

    Add isPro check in Info component

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,31 +1,44 @@
-"use client";
+'use client';
 
-import { Skeleton } from "@/components/ui";
-import { useOrganization } from "@clerk/nextjs";
-import { CreditCard } from "lucide-react";
-import Image from "next/image";
+import { Skeleton } from '@/components/ui';
+import { useOrganization } from '@clerk/nextjs';
+import { CreditCard } from 'lucide-react';
+import Image from 'next/image';
 
-function Info() {
+interface InfoProps {
+  isPro: boolean;
+}
+function Info({ isPro }: InfoProps) {
   const { organization, isLoaded } = useOrganization();
+
   if (!isLoaded) {
     return <Info.Skeleton />;
   }
   return (
-    <div className="flex items-center gap-x-4">
-      <div className="w-[60px] h-[60px] relative">
+    <div className='flex items-center gap-x-4'>
+      <div className='w-[60px] h-[60px] relative'>
         <Image
           fill
-          sizes="100%"
+          sizes='100%'
           src={organization?.imageUrl!}
-          alt="Organization"
-          className="rounded-md object-cover"
+          alt='Organization'
+          className='rounded-md object-cover'
         />
       </div>
-      <div className="space-y-1">
-        <p className="font-semibold text-xl">{organization?.name}</p>
-        <div className="flex items-center text-xs text-muted-foreground">
-          <CreditCard className="h-3 w-3 mr-1" />
-          Free
+      <div className='space-y-1'>
+        <p className='font-semibold text-xl'>{organization?.name}</p>
+        <div className='flex items-center text-xs text-muted-foreground'>
+          {isPro ? (
+            <span className='flex items-center py-1 px-1.5 bg-slate-100 text-slate-600 rounded-sm'>
+              <CreditCard className='h-3 w-3 mr-1' />
+              Pro
+            </span>
+          ) : (
+            <span className='flex items-center py-1 '>
+              <CreditCard className='h-3 w-3 mr-1' />
+              Free
+            </span>
+          )}
         </div>
       </div>
     </div>
@@ -34,15 +47,15 @@ function Info() {
 
 Info.Skeleton = function SkeletonInfo() {
   return (
-    <div className="flex items-center gap-x-4">
-      <div className="w-[60px] h-[60px] relative">
-        <Skeleton className="h-full w-full absolute" />
+    <div className='flex items-center gap-x-4'>
+      <div className='w-[60px] h-[60px] relative'>
+        <Skeleton className='h-full w-full absolute' />
       </div>
-      <div className="space-y-2">
-        <Skeleton className="h-10 w-[200px]" />
-        <div className="flex items-center">
-          <Skeleton className="h-4 w-4 mr-2" />
-          <Skeleton className="h-4 w-[100px]" />
+      <div className='space-y-2'>
+        <Skeleton className='h-10 w-[200px]' />
+        <div className='flex items-center'>
+          <Skeleton className='h-4 w-4 mr-2' />
+          <Skeleton className='h-4 w-[100px]' />
         </div>
       </div>
     </div>
--- app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -1,13 +1,15 @@
-import { Separator } from "@/components/ui";
-import Info from "../_components/info";
-import { Suspense } from "react";
-import ActivityList from "./_components/activity-list";
+import { Separator } from '@/components/ui';
+import Info from '../_components/info';
+import { Suspense } from 'react';
+import ActivityList from './_components/activity-list';
+import { checkSubscription } from '@/lib/subscription';
 
-function ActivityPage() {
+async function ActivityPage() {
+  const isPro = await checkSubscription();
   return (
-    <div className="w-full">
-      <Info />
-      <Separator className="my-4" />
+    <div className='w-full'>
+      <Info isPro={isPro} />
+      <Separator className='my-4' />
       <Suspense fallback={<ActivityList.Skeleton />}>
         <ActivityList />
       </Suspense>
