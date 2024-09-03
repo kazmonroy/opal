17c7a87e93c24162bd0028c1c88f194d56a60a77
kazmonroy <katherinemonroy@gmail.com>
2024-09-04 00:58:55 +0200

    Add Billing management feature

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -1,6 +1,7 @@
 import { checkSubscription } from '@/lib/subscription';
 import Info from '../_components/info';
 import { Separator } from '@/components/ui';
+import SubscriptionButton from './_components/subscription-button';
 
 async function BillingPage() {
   const isPro = await checkSubscription();
@@ -8,6 +9,7 @@ async function BillingPage() {
     <div className='w-full'>
       <Info isPro={isPro} />
       <Separator className='my-4' />
+      <SubscriptionButton isPro={isPro} />
     </div>
   );
 }
